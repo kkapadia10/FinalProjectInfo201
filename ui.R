fa6140fde9703aa191241bbb13c88e20fcf9c0b5
Kaden Kapadia <106453833+kkapadia10@users.noreply.github.com>
2023-03-10 21:48:36 -0800

    Completed project!

--- ui.R
+++ ui.R
@@ -36,26 +36,34 @@ ui <- fluidPage(
     tabPanel("Welcome",
              h2("Welcome to the Used and New Car Dataset"),
              imageOutput("welcome_image"),
-             h4("In this dataset, there are over", em("100,000 cars"), "for you to look at to figure out
-                which car fits your needs perfectly!"),
-             p("In the webapp, we are", strong("making it easier for buyers to find cars to purchase."), "We 
-               provide several car features so that the buyer can find the", em("perfect car that matches all their desired requirements.")),
-             p("Our target group would be", strong("people looking to purchase a new/used vehicle."), "This 
-               is because our web app makes it", em("simple and efficient for our buyers to find their ideal car.")),
-             p("This group  can use the data to make an informed decision about purchasing a new/used vehicle."),
-             p("This web app shows information regarding", em("various car data from", min(car_data$Year), "to", max(car_data$Year))),
-             p("There are", strong(format(nrow(car_data), big.mark = ","), "cars and", ncol(car_data), "variables") , "we will be looking at in this dataset."),
-             p("This data consists of several variables necessary for buyers to look at before buying cars, such as the",
-               em("Model, Year, Status, Mileage, Price, and MSRP.")),
-             p("This data is found on", strong("Kaggle"), "and was updated by", strong("George Baffour.")),
+             h4("In this dataset, there are over", em("100,000 cars"), "for you to look at to 
+             figure out which car fits your needs perfectly!"),
+             p("In the webapp, we are", strong("making it easier for buyers to find 
+             cars to purchase."), "We  provide several car features so that the buyer can find
+               the", em("perfect car that matches all their desired requirements.")),
+             p("Our target group would be", strong("people looking to purchase a new/used 
+             vehicle."), "This is because our web app makes it", 
+               em("simple and efficient for our buyers to find their ideal car.")),
+             p("This group  can use the data to make an informed decision about purchasing a 
+               new/used vehicle."),
+             p("This web app shows information regarding", em("various car data from", 
+                                                              min(car_data$Year), "to", 
+                                                              max(car_data$Year))),
+             p("There are", strong(format(nrow(car_data), big.mark = ","), 
+                                   "cars and", ncol(car_data), "variables") , 
+               "we will be looking at in this dataset."),
+             p("This data consists of several variables necessary for buyers to look at before
+               buying cars, such as the", em("Model, Year, Status, Mileage, Price, and MSRP.")),
+             p("This data is found on", strong("Kaggle"), "and was updated by", 
+               strong("George Baffour.")),
              h5("Below is a", em("random sample of 10 car data values:")),
              dataTableOutput("welcome_table"),
     ),
     
-    # This creates a scatter plot. The goal of the scatter plot, the actual scatter plot, instructions
-    # on how to use the scatter plot, and a summary of the scatter plot are all included.
-    # Additionally, the user has the option to select which car model's data to include
-    # in the scatter plot and the option to select a color for the data points. 
+    # This creates a scatter plot. The goal of the scatter plot, the actual scatter plot, 
+    # instructions on how to use the scatter plot, and a summary of the scatter plot are
+    # all included. Additionally, the user has the option to select which car model's 
+    # data to include in the scatter plot and the option to select a color for the data points.
     tabPanel("Scatter Plot",
              sidebarLayout(
                sidebarPanel(
@@ -140,25 +148,39 @@ ui <- fluidPage(
                column(width = 4, plotOutput("acura2_scatter")),
              h4("Analysis"),
              ),
-             p("Taking a look at the scatter plot tab, you can see the trend that as Mileage increases, the Price of the car
-               decreases. The above graphs (2022 Acura TLX A-Spec, 2023 BMW 330 i, and 2023 Acura MDX Type S) are good examples of this trend. The datapoints
-               above the blue trendline can be seen as overpriced; while, the datapoints below the trendline can be see as underpriced or good value purchases."),
-             p("Taking a look at the bar plot, we discovered a few trends. First, as the maximum mileage allowed increases, the price of the car decreases (the 
-             more miles on the car, the less it is worth). Next, the newer the car, the more expensive it is. In theory, if you wanted to find a more expensive
-               car, you would look at a newer car with low mileage (and the opposite for a cheaper car)."),
-             p("Finally, looking at the table, we can see all the cars of any given model. While this table does not provide us with a trend, it 
+             p("Taking a look at the scatter plot tab, you can see the trend that as Mileage 
+             increases, the Price of the car decreases. The above graphs 
+             (2022 Acura TLX A-Spec, 2023 BMW 330 i, and 2023 Acura MDX Type S) 
+             are good examples of this trend. The datapoints
+               above the blue trendline can be seen as overpriced; while, the datapoints below
+               the trendline can be see as underpriced or good value purchases."),
+             p("Taking a look at the bar plot, we discovered a few trends. First, as the maximum 
+             mileage allowed increases, the price of the car decreases (the 
+             more miles on the car, the less it is worth). Next, the newer the car, the more
+             expensive it is. In theory, if you wanted to find a more expensive
+               car, you would look at a newer car with low mileage (and the opposite for a 
+               cheaper car)."),
+             p("Finally, looking at the table, we can see all the cars of any given model. While 
+             this table does not provide us with a trend, it 
                is a great resource for buyers to use if they know which model car they want."),
              h4("Data Quality"),
-             p("In terms of data quality and reliability, the dataset does not include a few values from the Mileage, Price, and MSRP categories.
-              However, even with this the data is still very reliable because for every category a majority of the data is provided and accurate.
-               This dataset definitely provides unbaised results because there is no way to skew or lie about the Mileage, Price, Status, Year, etc. 
-               of a car. Additionally, there are no ethical issues with using this data as this is public information and no population groups were harmed to 
+             p("In terms of data quality and reliability, the dataset does not include a few 
+             values from the Mileage, Price, and MSRP categories. However, even with this the 
+             data is still very reliable because for every category a majority of the data is 
+             provided and accurate. This dataset definitely provides unbaised results because 
+             there is no way to skew or lie about the Mileage, Price, Status, Year, etc. 
+               of a car. Additionally, there are no ethical issues with using this data as this 
+               is public information and no population groups were harmed to 
               collect this data."),
              h4("Future Ideas"),
-             p("To advance this web app in the future, there are a few potential fixes or implementations. To start, we can add the dealer information for each car
-               so that the user can easily purchase the car if they desire. Additionally, another improvement would be to inclue reviews of the car from the previous
-               owners. If the car is used, we should try to get the previous owner to say what they liked/disliked about the car. Finally, we should try to expand
-               this web app to also look at motorcycles. Doing so would expand our target market to include those searching for a motorcycle.")
+             p("To advance this web app in the future, there are a few potential fixes or
+             implementations. To start, we can add the dealer information for each car
+               so that the user can easily purchase the car if they desire. Additionally,
+               another improvement would be to inclue reviews of the car from the previous
+               owners. If the car is used, we should try to get the previous owner to say 
+               what they liked/disliked about the car. Finally, we should try to expand
+               this web app to also look at motorcycles. Doing so would expand our target 
+               market to include those searching for a motorcycle.")
     ),
   )
 )
@@ -197,7 +219,8 @@ server <- function(input, output){
              Price = as.numeric(str_replace_all(Price, "[^[:digit:]]", ""))) %>%
       na.omit()
     # sets the plot to p (the slope line will be added to p)
-    p <- ggplot(model_data, aes(x = as.integer(gsub(" mi.", "", Mileage)), y = as.integer(gsub("$", "", Price)))) +
+    p <- ggplot(model_data, aes(x = as.integer(gsub(" mi.", "", Mileage)), 
+                                y = as.integer(gsub("$", "", Price)))) +
       # lets the user choose the color of the plot
       geom_point(col = input$scatter_color) +
       # adds the main title and axis titles
@@ -228,29 +251,35 @@ server <- function(input, output){
       filter(Model == input$model)
     n_total <- nrow(model_data)
     # If there is no available data, tell the user that a graph can not be created
-    if (is.na(max(model_data$Price)) || is.na(min(model_data$Price)) || is.na(max(model_data$Mileage)))
+    if (is.na(max(model_data$Price)) || is.na(min(model_data$Price)) || 
+        is.na(max(model_data$Mileage)))
     {
       paste0("There are ", n_total, " " , input$model, "s that have available data. 
           Due to this, we are unable to provide a graph comparing car price and mileage driven.
           However, check again later to see new information regarding the ", input$model, ".")
     }
-    # If there is enough data to create a graph, provide useful information so the user can understand 
-    # trends in the data set. Include information regarding the most and least expensive car, and the 
-    # highest mileage car for a certain model. Additionally, include slope informaiton and analysis to
-    # reveal the trend in the data.
+    # If there is enough data to create a graph, provide useful information so the user can 
+    # understand trends in the data set. Include information regarding the most and least 
+    # expensive car, and the highest mileage car for a certain model. Additionally, include 
+    # slope information and analysis to reveal the trend in the data.
     else
     {
-      output_text <- paste0("There are ", n_total, " " , input$model, "s that have available data. ",
-                            "The most expensive one costs ", max(model_data$Price), " and the cheapest one costs ", min(model_data$Price), 
-                            ". The ", input$model, " with the highest mileage has driven ", max(model_data$Mileage))
+      output_text <- paste0("There are ", n_total, " " , input$model, 
+                            "s that have available data. ",
+                            "The most expensive one costs ", max(model_data$Price), 
+                            " and the cheapest one costs ", min(model_data$Price), 
+                            ". The ", input$model, " with the highest mileage has driven ",
+                            max(model_data$Mileage))
       if (!is.null(slope) && slope < 0)
       {
         output_text <- paste0(output_text, " Given that we have a negative slope of ", slope, 
-                              ", we can see the general trend that as the mileage increases, the price decreases.")
+                              ", we can see the general trend that as the mileage increases, 
+                              the price decreases.")
       }
       if (!is.null(slope) && slope >= 0)
       {
-        output_text <- paste0(output_text, " Given that we have a positive slope of ", slope, ", the ", input$model, " goes against the general trend that 
+        output_text <- paste0(output_text, " Given that we have a positive slope of ", slope, ", 
+                              the ", input$model, " goes against the general trend that 
                               as the mileage increases, the price decreases.")
       }
       output_text
@@ -259,7 +288,8 @@ server <- function(input, output){
   
   # goal of the scatter plot
   output$scatter_goal <- renderText({
-    paste("The goal of this scatter plot is to examine the relationship beween price and mileage for various car models.")
+    paste("The goal of this scatter plot is to examine the relationship beween price and mileage 
+          for various car models.")
   })
   
   # instructions on how to use the scatter plot
@@ -268,10 +298,11 @@ server <- function(input, output){
     adjust what the plot shows. The first tool (top to bottom) is to adjust the model of the 
     car you’re interested in (the list is alphabetical so the first brand is Acura and so on). 
     The second tool is used to adjust the color of the points (cars) on the graph and has no 
-    impact on the data that is shown. On the graph, there is a trend line (the blue line going through the middle). 
-    This shows a rough average of the price of the car given the model and mileage. A useful way to interpret the data 
-    is that any car above the trend line is overpriced compared to the average, and similarly, any data under the trend 
-    line would be considered a good deal and have a lower price than average.")
+    impact on the data that is shown. On the graph, there is a trend line (the blue line going 
+    through the middle). This shows a rough average of the price of the car given the model and
+    mileage. A useful way to interpret the data is that any car above the trend line is overpriced 
+    compared to the average, and similarly, any data under the trend line would be considered a 
+          good deal and have a lower price than average.")
   })
   
   # Define the bar plot output
@@ -314,26 +345,30 @@ server <- function(input, output){
     # Provides information about the number of cars that have at most the user inputted mileage.
     # Additionally, provides the average value of the car for the chosen mileage and year.
     paste0(format(n_non_missing, big.mark = ","), " cars have at most ", 
-          format(input$mileage, big.mark = ","), " miles. For ", input$year_bar,", the average price of cars under ", 
-          format(input$mileage, big.mark = ","), " miles is $", format(round(avg_price), nsmall = 0, big.mark = ","), ". 
-          As the maximum mileage allowed increases, the price of the car decreases (the more miles on the car, the less it is worth).
-           Next, the newer the car, the more expensive it is.")
+          format(input$mileage, big.mark = ","), " miles. For ", input$year_bar,", the average 
+          price of cars under ", format(input$mileage, big.mark = ","), " miles is $", 
+          format(round(avg_price), nsmall = 0, big.mark = ","), ". As the maximum mileage allowed
+          increases, the price of the car decreases (the more miles on the car, the less it 
+          is worth). Next, the newer the car, the more expensive it is.")
   })
   
   # goal of the bar plot
   output$bar_goal <- renderText({
-    paste("The goal of this bar plot is to examine the relationship between price and manufacture year.")
+    paste("The goal of this bar plot is to examine the relationship between price 
+          and manufacture year.")
   })
   
   # instructions for the bar plot
   output$bar_instructions <- renderText({
-    paste("Instructions: On the left side of the page, there are three tools you can use to adjust what the plot shows. 
-          The first tool (top to bottom) is to adjust the maximum mileage a car can have. In order to change this value 
-          slide the dot right and left (the minimum is 0 miles and the maximum is 150,000 miles). The second tool 
-          (the middle) can be used to determine the year of the car you are interested in. Similar to the mileage tool, 
-          slide the dot right and left to change the year of interest (the oldest models are 1943 and the newest is 2023). Changing the second tool,
-          changes the year we are looking at to find the average car value.
-          The last tool simply changes the color of the bars on the plot and has no effect on the data about the cars that are being presented.")
+    paste("Instructions: On the left side of the page, there are three tools you can use to 
+    adjust what the plot shows. The first tool (top to bottom) is to adjust the maximum mileage 
+    a car can have. In order to change this value slide the dot right and left (the minimum 
+    is 0 miles and the maximum is 150,000 miles). The second tool (the middle) can be used 
+    to determine the year of the car you are interested in. Similar to the mileage tool, 
+    slide the dot right and left to change the year of interest (the oldest models are 
+    1943 and the newest is 2023). Changing the second tool, changes the year we are looking 
+    at to find the average car value. The last tool simply changes the color of the bars 
+          on the plot and has no effect on the data about the cars that are being presented.")
   })
   
   # Define the table output
@@ -364,14 +399,16 @@ server <- function(input, output){
   # instructions for the table
   output$table_instructions <- renderText({
     paste("Instructions: Slide the slider to the desired minimum year to be displayed on the table.
-    The table will then display the following information about the cars: Year, Status, Mileage, Price, and MSRP.
-    You can also elect how many entries you want in the table. You can select either 10, 25, 50, and 100 entries to be displayed.
+    The table will then display the following information about the cars: Year, Status, Mileage, 
+    Price, and MSRP. You can also elect how many entries you want in the table. You can select 
+    either 10, 25, 50, and 100 entries to be displayed.
           The Model will display the model of the vehicle, 
           Year displays the year of the vehicle, 
           Status will display whether the vehicle is used or new,
           Mileage displays the mileage of the vehicle,
           Price displays the price of the vehicle,
-          and MSRP specifies the drop or increase in the price of the vehicle’s price according to the manufacturer.")
+          and MSRP specifies the drop or increase in the price of the vehicle’s price according
+          to the manufacturer.")
   })
   
   # Scatter plot for the 2022 Acura TLX A-Spec data.
